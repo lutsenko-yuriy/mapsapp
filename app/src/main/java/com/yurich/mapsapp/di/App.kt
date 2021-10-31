fd74afc81eda3025f57e5ede0b02b386d3b23540
lutsenko-yuriy <lutsenko.yuriy93@gmail.com>
2021-10-31 16:31:14 +0100

    Importing Hilt

---
+++ app/src/main/java/com/yurich/mapsapp/di/App.kt
@@ -0,0 +1,11 @@
+package com.yurich.mapsapp.di
+
+import androidx.multidex.MultiDexApplication
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : MultiDexApplication() {
+
+
+
+}
