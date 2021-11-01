88d2e5e63088a2f5ce5f59420a766201f12b64f9
lutsenko-yuriy <lutsenko.yuriy93@gmail.com>
2021-11-01 01:06:40 +0100

    Maps

--- app/src/main/java/com/yurich/mapsapp/presentation/map/MapFragment.kt
+++ app/src/main/java/com/yurich/mapsapp/presentation/map/MapFragment.kt
@@ -1,13 +1,17 @@
 package com.yurich.mapsapp.presentation.map
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.yurich.mapsapp.R
+import com.yurich.mapsapp.domain.Vehicle
 import com.yurich.mapsapp.presentation.main.models.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,6 +24,8 @@ class MapFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
 
+    private lateinit var mapView: MapView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,10 +35,36 @@ class MapFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
         viewModel = ViewModelProvider(this.requireActivity())[MainViewModel::class.java]
 
-        viewModel.viewState.observe(viewLifecycleOwner) {
-            Log.v("MapFragment", "Selected ${it.selectedVehicle} among ${it.availableVehicles.size}")
+        initializeMap()
+    }
+
+    private fun initializeMap() {
+        val view = this.view ?: return
+
+        mapView = view.findViewById(R.id.map_view)
+        mapView.getMapAsync { map ->
+            viewModel.viewState.observe(viewLifecycleOwner) {
+                map.clear()
+                if (it.selectedVehicle != null) {
+                    drawMarkerForVehicle(map, it.selectedVehicle)
+                } else {
+                    for (vehicle in it.availableVehicles) {
+                        drawMarkerForVehicle(map, vehicle)
+                    }
+                }
+            }
         }
     }
+
+    private fun drawMarkerForVehicle(map: GoogleMap, vehicle: Vehicle) {
+        map.addMarker(
+            MarkerOptions()
+                .position(LatLng(vehicle.coordinates.latitude, vehicle.coordinates.longitude))
+                .title(vehicle.name)
+        )
+    }
+
 }
